9b36ec42f9a730d8c074500dae131558e2ccf4d2
zhengwhizz <zhengjun.whizz@gmail.com>
2020-06-16 10:30:46 +0800

    helper doc

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,12 @@
 
 if (!function_exists('replace_placeholder')) {
 
+    /**
+     * 使用正则替换点位符，占位符里不能出现正则的特殊字符，
+     * @param string $subject 要替换的文本
+     * @param array $data 占位符及数据的数组
+     * @return string
+     */
     function replace_placeholder($subject, $data): string
     {
         return preg_replace(array_map(function ($item) {
